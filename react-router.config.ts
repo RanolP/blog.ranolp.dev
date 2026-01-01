c3dfda74b5f0a46318c5a6288562123c7d15cc25
RanolP <me@ranolp.dev>
2026-01-02 03:21:30 +0900

    Refactor build and development scripts in package.json to support server-side rendering. Update SSR configuration in react-router.config.ts to utilize environment variables for better flexibility. Add multiple tweet JSON files for enhanced content management and introduce new API routes for image upload and metadata fetching, improving overall functionality and user experience.

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import { getPublishedPosts } from './src/services/posts/repository.server';
 
 export default {
   appDirectory: 'src/app',
-  ssr: true,
+  ssr: process.env.IS_SSR === 'true',
   async prerender() {
     const posts = await getPublishedPosts();
     const postPaths = posts.map((post) => `/post/${post.slug}`);
